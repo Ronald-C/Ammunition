126b4bc45cbb4e32e7aa95f4f27ade8c0185b4e8
Ronald Cheng <ronaldcheng14@gmail.com>
2016-08-27 03:11:08 -0700

    setup.py creation

---
+++ setup.py
@@ -0,0 +1,41 @@
+"""
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+
+"""
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+try:
+	with open(path.join(here, 'README.md'), 'rb') as readFile:
+		long_description = f.read()
+
+except Exception:
+	long_description = ''
+
+
+setup(
+	name='roomGrab',
+	version='2.0.0',
+	description=long_description,
+	url='',
+	
+	author='Ronald',
+	author_email='',
+
+	license='MIT',
+
+	packages=find_packages(exclude=[Automation.test]),
+	install_requires=['requires', 'bs4'],
+	entry_points={
+		'console_scripts' : [
+			'app'='Automation:main'
+		]
+	},
+
+)
